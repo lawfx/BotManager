707c0e10a8e9c7a3aba0d4044e44c06d82b0fba5
Nikolaos Ioannou <ioannounikos93@gmail.com>
2019-09-02 20:32:57 +0200

    initial commit
    initial commit

---
+++ server/src/server.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+import path from 'path';
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, '../../client/dist/client/')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../client/dist/client/index.html'));
+});
+
+app.listen(9000, () => console.log('Server running in port 9000'));
