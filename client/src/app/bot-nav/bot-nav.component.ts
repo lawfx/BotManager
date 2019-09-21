f9d91416a7fd69e68fa47be6d16a3d882dc918af
Nikolaos Ioannou <ioannounikos93@gmail.com>
2019-09-22 01:57:09 +0200

    added real bot names and avatars to navbar by requesting them - made active nav tab to have full opacity when active

--- client/src/app/bot-nav/bot-nav.component.ts
+++ client/src/app/bot-nav/bot-nav.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { BotService } from '../bot.service';
+import { BotInfo } from '../interfaces';
 
 @Component({
   selector: 'app-bot-nav',
@@ -6,10 +8,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./bot-nav.component.css']
 })
 export class BotNavComponent implements OnInit {
+  redAlert = {} as BotInfo;
+  janusz = {} as BotInfo;
 
-  constructor() { }
+  constructor(private botService: BotService) {}
 
   ngOnInit() {
+    this.getBotsInfo();
   }
 
+  private getBotsInfo() {
+    this.botService.getInfo('redalert').subscribe({
+      next: (botInfo: BotInfo) => (this.redAlert = botInfo)
+    });
+    this.botService.getInfo('janusz').subscribe({
+      next: (botInfo: BotInfo) => (this.janusz = botInfo)
+    });
+  }
 }
